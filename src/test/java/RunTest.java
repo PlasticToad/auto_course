3d1722f82a0f5c2b99c54a29b127cd92a2b4bb0b
IBorobov <ilya.borobov@ifellow.ru>
2023-10-11 15:22:20 +1000

    Commit 24. HW6_API. API minor changes

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,23 +1,27 @@
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static api.ApiSteps.*;
+@Epic("Курсы АТ")
+@Feature("Тестирование API")
+@DisplayName("Тестирование API")
 
 public class RunTest {
-    @Epic("Рик и Морти api")
-    @Feature("Сравнение расы и локации двух персонажей")
-    @Owner("Боробов")
+
     @Test
+    @DisplayName("Рик и Морти api")
+    @Owner("Боробов")
     public void task1() {
         checkSpeciesLocation("2");//ID Морти(2)
     }
 
-    @Epic("reqres.in api")
-    @Feature("Создание пользователя + проверка ответа")
-    @Owner("Боробов")
+
     @Test
+    @DisplayName("reqres.in api")
+    @Owner("Боробов")
     public void task2() {
         createUser("Tomato", "Eat market");
     }
